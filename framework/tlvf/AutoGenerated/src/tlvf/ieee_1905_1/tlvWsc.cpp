7943099485762a7c41c04c4014eb48ea5fe538c6
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-12-16 09:08:56 -0800

    tlvf: add tlvWsc.yaml
    Preparative commit.
    
    The WSC TLVs are regular TLVs which contain a list of WSC Attributes,
    where each attribute is of a TLV structure where both length and type
    are 2 bytes long.
    
    The current implementation defines 2 yaml files for 2 types of WSC TLVs
    - WSC M2 and WSC M1. Each of those yaml files contain the list of
    attributes (defined in WSC_Attributes.yaml) in strict ordering, meaning
    that if we get a WSC M2 or M1 TLV with different attributes order then
    we will fail on parsing.
    
    This is by itself wrong, and the reason that prplMesh cannot currently
    pass autoconfiguration with most controller when acting as agent.
    
    Previous PR (#572) added support for TlvList class, which can be used to
    describe a list of TLV like structures inside an outer TLV.
    The PR did it for vendor specific Intel messages by adding
    beerocks_header which derives from TlvList.
    
    The same solution can now be used to fix the ordering issue with the WSC
    TLVs - by defining a new WSC TLV yaml with an unknown length list of
    uint8_t which will be used as the buffer for the inner TlvList.
    
    This commit adds WscTlv.yaml which provides just that. Followup commits
    will use this to acheive the final goal of replacing tlvWscM1.yaml and
    tlvWscM2.yaml with the wscTlv.yaml and a derived class of TlvList which
    will provide parsing and create APIs for both M2 and M1 WSC types.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

---
+++ framework/tlvf/AutoGenerated/src/tlvf/ieee_1905_1/tlvWsc.cpp
@@ -0,0 +1,126 @@
+///////////////////////////////////////
+// AUTO GENERATED FILE - DO NOT EDIT //
+///////////////////////////////////////
+
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ * Copyright (c) 2016-2019 Intel Corporation
+ *
+ * This code is subject to the terms of the BSD+Patent license.
+ * See LICENSE file for more details.
+ */
+
+#include <tlvf/ieee_1905_1/tlvWsc.h>
+#include <tlvf/tlvflogging.h>
+
+using namespace ieee1905_1;
+
+tlvWscM2::tlvWscM2(uint8_t* buff, size_t buff_len, bool parse, bool swap_needed) :
+    BaseClass(buff, buff_len, parse, swap_needed) {
+    m_init_succeeded = init();
+}
+tlvWscM2::tlvWscM2(std::shared_ptr<BaseClass> base, bool parse, bool swap_needed) :
+BaseClass(base->getBuffPtr(), base->getBuffRemainingBytes(), parse, swap_needed){
+    m_init_succeeded = init();
+}
+tlvWscM2::~tlvWscM2() {
+}
+const eTlvType& tlvWscM2::type() {
+    return (const eTlvType&)(*m_type);
+}
+
+const uint16_t& tlvWscM2::length() {
+    return (const uint16_t&)(*m_length);
+}
+
+uint8_t* tlvWscM2::payload(size_t idx) {
+    if ( (m_payload_idx__ <= 0) || (m_payload_idx__ <= idx) ) {
+        TLVF_LOG(ERROR) << "Requested index is greater than the number of available entries";
+        return nullptr;
+    }
+    return &(m_payload[idx]);
+}
+
+bool tlvWscM2::alloc_payload(size_t count) {
+    if (m_lock_order_counter__ > 0) {;
+        TLVF_LOG(ERROR) << "Out of order allocation for variable length list payload, abort!";
+        return false;
+    }
+    if (count == 0) {
+        TLVF_LOG(WARNING) << "can't allocate 0 bytes";
+        return false;
+    }
+    size_t len = sizeof(uint8_t) * count;
+    if(getBuffRemainingBytes() < len )  {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer - can't allocate";
+        return false;
+    }
+    m_lock_order_counter__ = 0;
+    uint8_t *src = (uint8_t *)m_payload;
+    uint8_t *dst = src + len;
+    if (!m_parse__) {
+        size_t move_length = getBuffRemainingBytes(src) - len;
+        std::copy_n(src, move_length, dst);
+    }
+    m_payload_idx__ += count;
+    if (!buffPtrIncrementSafe(len)) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << len << ") Failed!";
+        return false;
+    }
+    if(m_length){ (*m_length) += len; }
+    return true;
+}
+
+void tlvWscM2::class_swap()
+{
+    tlvf_swap(16, reinterpret_cast<uint8_t*>(m_length));
+}
+
+size_t tlvWscM2::get_initial_size()
+{
+    size_t class_size = 0;
+    class_size += sizeof(eTlvType); // type
+    class_size += sizeof(uint16_t); // length
+    return class_size;
+}
+
+bool tlvWscM2::init()
+{
+    if (getBuffRemainingBytes() < get_initial_size()) {
+        TLVF_LOG(ERROR) << "Not enough available space on buffer. Class init failed";
+        return false;
+    }
+    m_type = (eTlvType*)m_buff_ptr__;
+    if (!m_parse__) *m_type = eTlvType::TLV_WSC;
+    if (!buffPtrIncrementSafe(sizeof(eTlvType))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(eTlvType) << ") Failed!";
+        return false;
+    }
+    m_length = (uint16_t*)m_buff_ptr__;
+    if (!m_parse__) *m_length = 0;
+    if (!buffPtrIncrementSafe(sizeof(uint16_t))) {
+        LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << sizeof(uint16_t) << ") Failed!";
+        return false;
+    }
+    m_payload = (uint8_t*)m_buff_ptr__;
+    if (m_length && m_parse__) {
+        size_t len = *m_length;
+        if (m_swap__) { tlvf_swap(16, reinterpret_cast<uint8_t*>(&len)); }
+        len -= (m_buff_ptr__ - sizeof(*m_type) - sizeof(*m_length) - m_buff__);
+        m_payload_idx__ = len/sizeof(uint8_t);
+        if (!buffPtrIncrementSafe(len)) {
+            LOG(ERROR) << "buffPtrIncrementSafe(" << std::dec << len << ") Failed!";
+            return false;
+        }
+    }
+    if (m_parse__ && m_swap__) { class_swap(); }
+    if (m_parse__) {
+        if (*m_type != eTlvType::TLV_WSC) {
+            TLVF_LOG(ERROR) << "TLV type mismatch. Expected value: " << int(eTlvType::TLV_WSC) << ", received value: " << int(*m_type);
+            return false;
+        }
+    }
+    return true;
+}
+
+
